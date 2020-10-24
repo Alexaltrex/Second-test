08a32c84c6c8b8dc58980d698935574239196371
Alexaltrex <alexaltrex@mail.ru>
2020-10-24 23:23:49 +0400

    Updates

---
+++ precache-manifest.6cd50ad9bce7cad90207e5b855ea288b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb97cbde10494135db9e17df527518d6",
+    "url": "/Second-test/index.html"
+  },
+  {
+    "revision": "62a80a5ed578b2e7958a",
+    "url": "/Second-test/static/js/2.338469d1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Second-test/static/js/2.338469d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88963ca93e08e18699e8",
+    "url": "/Second-test/static/js/main.32bc5a5a.chunk.js"
+  },
+  {
+    "revision": "6d09eedd4aad61025000",
+    "url": "/Second-test/static/js/runtime-main.eae45975.js"
+  }
+]);
